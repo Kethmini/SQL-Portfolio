aab30190de236ffbb0c868160e3ffd96e6e22be3
Kethmini Rupasinghe <61416093+Kethmini@users.noreply.github.com>
2023-07-10 00:53:52 +0530

    Add files via upload

---
+++ booklist.sql
@@ -0,0 +1,13 @@
+Challenge: Book list database
+
+Create a database table to store favorite books.
+
+CREATE TABLE favbooks(id INTEGER PRIMARY KEY, name TEXT, rating INTEGER);
+
+Add three of your favorite books into the table.
+
+INSERT INTO favbooks VALUES (1, "Matilda", 5);
+INSERT INTO favbooks VALUES (2, "Secret Garden", 4);
+INSERT INTO favbooks VALUES (3, "Black Beauty", 3);
+
+
